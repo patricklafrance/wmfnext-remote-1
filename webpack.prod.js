05f7866126772a27f5cb3b7a82d0614291c751be
patricklafrance <pa.lafrance@gmail.com>
2023-01-27 17:59:02 -0500

    Fixed webpack config

--- webpack.prod.js
+++ webpack.prod.js
@@ -55,13 +55,13 @@ export default {
         ]
     },
     resolve: {
-        alias: !isNetlify && {
+        alias: !isNetlify ? {
             // Without the aliases, at runtime an "Invalid hook call" is thrown, see https://stackoverflow.com/questions/64283813/invalid-hook-call-on-npm-link-library
             "react": path.resolve(__dirname, "./node_modules/react"),
             "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
             // Without the alias, at runtime the index route doesn't render and we are stuck with a blank page.
             "react-router-dom": path.resolve(__dirname, "./node_modules/react-router-dom")
-        },
+        } : {},
         // Must add ".js" for files imported from node_modules.
         extensions: [".js", ".ts", ".tsx", ".css"]
     },
